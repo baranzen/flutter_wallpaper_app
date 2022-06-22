5da3cd4367d756dd01752b3337f3d3c531341671
Baran <baranzengeralp@gmail.com>
2022-06-23 02:36:17 +0300

    initial commit

--- lib/pages/favorite_page.dart
+++ lib/pages/favorite_page.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: must_be_immutable
 
 import 'package:flutter/material.dart';
-import 'package:wallpaper_app/pages/home_page.dart';
 
 class FavoritePage extends StatefulWidget {
   FavoritePage(this.likedList, {Key? key}) : super(key: key);
@@ -51,7 +50,7 @@ class _FavoritePageState extends State<FavoritePage> {
       ],
       onTap: (index) {
         if (index == 0) {
-          ; //!push yapınca ltekrar gelince listeler gözükmüyor
+          //!push yapınca ltekrar gelince listeler gözükmüyor
           //!pop yap ki listeler tekrardan gözüksün
           Navigator.pop(context);
         }
